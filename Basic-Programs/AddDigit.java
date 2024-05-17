1b85a0218caed139559df82a3b8f1d8c68b95d14
Shivani yadav <94887322+Shivaniyadav2910@users.noreply.github.com>
2024-05-17 20:24:20 +0530

    AddDigit.java

---
+++ Basic-Programs/AddDigit.java
@@ -0,0 +1,19 @@
+
+    /*AddDigit */
+   static int addDigit(int n){
+        int sum=0;
+        for(int i=0; i<=n; i++){
+            int digit=n%10;
+            sum =sum+digit;
+            n=n/10;
+        }
+        return sum;
+    }
+
+
+public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(addDigit(n
+
+                     }                                    }
